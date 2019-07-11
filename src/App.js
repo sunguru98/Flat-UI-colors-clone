c56393b1af0e31af0deb368fcb633fd362c7d35e
sundeep <sunguru98@yahoo.co.in>
2019-07-11 19:23:34 +0530

    Added react router and created two routes for palette list and palette colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import ColorPalette from './ColorPalette'
 import seedPalettes from './seedPalettes'
 import generateColorBreakpoints from './generateColorBreakpoints'
 
+const findPalette = paletteId => {
+  return generateColorBreakpoints(seedPalettes.find(palette => palette.id === paletteId))
+}
+
 function App() {
-  console.log(generateColorBreakpoints(seedPalettes[4]))
   return (
     <div className="App">
-      <ColorPalette pallete={generateColorBreakpoints(seedPalettes[3])}/>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Hi there</h1>}/>
+        <Route exact path='/palette/:id' render={routeParams => <ColorPalette pallete={findPalette(routeParams.match.params.id)}/>}/>
+      </Switch>
     </div>
   );
 }
--- src/ColorPalette.js
+++ src/ColorPalette.js
@@ -4,7 +4,7 @@ import ColorPaletteNavbar from './ColorPaletteNavbar'
 
 class ColorPalette extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = { colorLevel: 500, colorFormat: 'hex#' }
     this.handleChange = this.handleChange.bind(this)
     this.changeColorFormat = this.changeColorFormat.bind(this)
@@ -25,7 +25,10 @@ class ColorPalette extends Component {
         <div className="ColorPalette-colors" style={{ height: '90%' }}>
           {this.props.pallete.colors[this.state.colorLevel].map(color => <ColorBox key={color.name} color={color[this.state.colorFormat]} name={color.name}/>)}
         </div>
-
+        <div className="ColorPalette-footer" style={{ height: '4vh', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', fontWeight: 'bold' }}>
+          {this.props.pallete.paletteName}
+          <span className="ColorPalette-emoji" style={{fontSize: '20px', margin: '0 5px'}}>{this.props.pallete.emoji}</span>
+        </div> 
       </div>
     );
   }
